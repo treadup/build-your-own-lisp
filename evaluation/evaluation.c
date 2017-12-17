407878ffc30b33f22b8aa1f962d0218b0d7d8e98
Henrik Treadup <henrik.treadup@gmail.com>
2017-12-17 16:00:36 +0000

    Evaluate program.

--- evaluation/evaluation.c
+++ evaluation/evaluation.c
@@ -6,6 +6,38 @@
 
 #include "mpc.h"
 
+/* Use operator string to see which operator to use */
+long eval_op(long x, char* op, long y) {
+    if(strcmp(op, "+") == 0) { return x + y; }
+    if(strcmp(op, "-") == 0) { return x - y; }
+    if(strcmp(op, "*") == 0) { return x * y; }
+    if(strcmp(op, "/") == 0) { return x / y; }
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+
+    /* If tagged as number return it directly */
+   if(strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    /* The operator is always the second child */
+    char* op = t->children[1]->contents;
+
+    /* We store the third child in the variable x */
+    long x = eval(t->children[2]);
+
+    /* Iterate the remaining children and combining */
+    int i = 3;
+    while(strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    return x;
+}
+
 int main(int argc, char** argv) {
 
     /* Create some parsers */
@@ -43,8 +75,9 @@ int main(int argc, char** argv) {
         /* Attempt to parse the user input */
         mpc_result_t r;
         if(mpc_parse("<stdin>", input, Lispy, &r)) {
-            /* On Success Print the AST */
-            mpc_ast_print(r.output);
+            /* On Success Evaluate the AST */
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
         } else {
             /* Otherwise Print the Error */

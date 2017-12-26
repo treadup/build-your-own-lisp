0c0bf1ef55bf2d35d088661f108231a93e9b01f2
Henrik Treadup <henrik.treadup@gmail.com>
2017-12-26 13:14:11 +0000

    Finihsed adding Q-expressions.

--- qexps/qexps.c
+++ qexps/qexps.c
@@ -28,7 +28,7 @@ enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
 lval* lval_eval(lval* v);
 lval* builtin_op(lval* a, char *op);
-
+lval* builtin(lval* a, char* func);
 
 /* Construct a pointer to a new Number lval */
 lval* lval_num(long x) {
@@ -261,7 +261,7 @@ lval* lval_eval_sexpr(lval* v) {
     }
 
     /* Call builtin with operator */
-    lval* result = builtin_op(v, f->sym);
+    lval* result = builtin(v, f->sym);
     lval_del(f);
     return result;
 }
@@ -274,6 +274,8 @@ lval* lval_eval(lval* v) {
         return v;
     }
 }
+
+#define LASSERT(args, cond, err) if(!(cond)) { lval_del(args); return lval_err(err); }
 
 lval* builtin_op(lval* a, char *op) {
 
@@ -317,6 +319,98 @@ lval* builtin_op(lval* a, char *op) {
     return x;
 }
 
+lval* builtin_head(lval* a) {
+    LASSERT(a, a->count == 1, "Function 'head' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+        "Function 'head' passed incorrect types");
+    LASSERT(a, a->cell[0]->count > 0,
+        "Function 'head' passed {}");
+
+    /* Otherwise take first argument */
+    lval* v = lval_take(a, 0);
+
+    /* Delete all elements that are not head and return */
+    while(v->count > 1) {
+        lval_del(lval_pop(v, 1));
+    }
+
+    return v;
+}
+
+lval* builtin_tail(lval* a) {
+    LASSERT(a, a->count == 1,
+        "Function 'tail' passed too many arguments.");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+        "Function 'tail' was passed incorrect types.");
+    LASSERT(a, a->cell[0]->count > 0,
+        "Function 'tail' passed {}");
+
+    /* Take the first argument */
+    lval* v = lval_take(a, 0);
+
+    /* Delete first element and return */
+    lval_del(lval_pop(v, 0));
+
+    return v;
+}
+
+lval* builtin_list(lval* a) {
+    a->type = LVAL_QEXPR;
+    return a;
+}
+
+lval* builtin_eval(lval* a) {
+    LASSERT(a, a->count == 1,
+            "Function 'eval' passed too many arguments");
+    LASSERT(a, a->cell[0]->type == LVAL_QEXPR,
+            "Function 'eval' passed incorrect type");
+
+    lval* x = lval_take(a, 0);
+    x->type = LVAL_SEXPR;
+    return lval_eval(x);
+}
+
+lval* lval_join(lval* x, lval* y) {
+
+    /* For each cell in 'y' add it to 'x' */
+    while(y->count) {
+        x = lval_add(x, lval_pop(y, 0));
+    }
+
+    /* Delete the empty y and return x */
+    lval_del(y);
+
+    return x;
+}
+
+lval* builtin_join(lval* a) {
+
+    for (int i = 0; i < a->count; i++) {
+        LASSERT(a, a->cell[i]->type == LVAL_QEXPR,
+                "Function 'join' passed incorrect type.");
+    }
+
+    lval* x = lval_pop(a, 0);
+
+    while(a->count) {
+        x = lval_join(x, lval_pop(a, 0));
+    }
+
+    lval_del(a);
+
+    return x;
+}
+
+lval* builtin(lval* a, char* func) {
+    if(strcmp("list", func) == 0) { return builtin_list(a); }
+    if(strcmp("head", func) == 0) { return builtin_head(a); }
+    if(strcmp("tail", func) == 0) { return builtin_tail(a); }
+    if(strcmp("join", func) == 0) { return builtin_join(a); }
+    if(strcmp("eval", func) == 0) { return builtin_eval(a); }
+    if(strstr("+-/*", func)) { return builtin_op(a, func); }
+    lval_del(a);
+    return lval_err("Unknown function");
+}
 
 int main(int argc, char** argv) {
     printf("Welcome to Lispy 0.1\n");
@@ -332,7 +426,9 @@ int main(int argc, char** argv) {
     mpca_lang(MPCA_LANG_DEFAULT,
         "                                                    \
           number : /-?[0-9]+/ ;                              \
-          symbol : '+' | '-' | '*' | '/' ;                   \
+          symbol : \"list\" | \"head\" | \"tail\" |          \
+                   \"join\" | \"eval\" |                     \
+                   '+' | '-' | '*' | '/' ;                   \
           sexpr  : '(' <expr>* ')' ;                         \
           qexpr  : '{' <expr>* '}' ;                         \
           expr   : <number> | <symbol> | <sexpr> | <qexpr> ; \
